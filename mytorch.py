1b06770f548d407d240c25585e6b0d9d79029d02
Kiran Praveen <kirant1994@gmail.com>
2020-02-15 22:53:13 +0530

    Fixing multiline comments

--- mytorch.py
+++ mytorch.py
@@ -34,14 +34,12 @@ class DataParallel(torch.nn.DataParallel):
     def load(self, path):
         _load_model(self.module, path)
 
-"""
-Chunk a batch of sequences.
-Inputs : Tensor of shape (batch_size, time_steps, dimension),
-		 left_context, mid_context, right_context
-Outputs : Chunked input tensor, original time
-Using strict chunking avoids the assumption that the sequence is circular,
-but impacts the performance for longer length sequences by a slight margin.
-""" 
+# Chunk a batch of sequences.
+# Inputs : Tensor of shape (batch_size, time_steps, dimension),
+#		   left_context, mid_context, right_context
+# Outputs : Chunked input tensor, original time
+# Using strict chunking avoids the assumption that the sequence is circular,
+# but impacts the performance for longer length sequences by a slight margin.
 def chunk(X, left, mid, right, strict=False):
     if left > mid or right > mid:
         return _chunk_extended(X, left, mid, right, strict=strict)
@@ -63,10 +61,8 @@ def chunk(X, left, mid, right, strict=False):
         X = X[:, 1:-1, :, :].contiguous()
     return X, t
 
-"""
-Extended support for chunking when left or right > mid.
-Not using this by default because using lists worsen the performance by a small amount.
-"""
+# Extended support for chunking when left or right > mid.
+# Not using this by default because using lists worsen the performance by a small amount.
 def _chunk_extended(X, left, mid, right, strict=False):
     N, t, d = X.shape
     n_chunks = int(np.ceil(float(t) / mid))
@@ -88,11 +84,9 @@ def _chunk_extended(X, left, mid, right, strict=False):
     	X = X[:, left_reps:n_chunks+left_reps, :, :]
     return X, t
 
-"""
-Inverse of the chunking operation.
-Inputs : chunked tensor from chunk() function, left_context, mid_context, right_context, original_time
-Output : Tensor of shape same as input to the chunk() function
-"""
+# Inverse of the chunking operation.
+# Inputs : chunked tensor from chunk() function, left_context, mid_context, right_context, original_time
+# Output : Tensor of shape same as input to the chunk() function
 def unchunk(X, left, mid, right, t_original):
     N, n_chunks, size_chunk, d = X.shape
     X = X[:, :, left:left+mid, :].contiguous()
