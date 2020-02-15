889a82322e53ad6d37a345be12ec2c743beb6045
Kiran Praveen <kirant1994@gmail.com>
2020-02-15 23:22:51 +0530

    Handling squeezed single dimensional frames.

--- mytorch.py
+++ mytorch.py
@@ -36,13 +36,18 @@ class DataParallel(torch.nn.DataParallel):
 
 # Chunk a batch of sequences.
 # Inputs : Tensor of shape (batch_size, time_steps, dimension),
-#		   left_context, mid_context, right_context
+#           left_context, mid_context, right_context
 # Outputs : Chunked input tensor, original time
 # Using strict chunking avoids the assumption that the sequence is circular,
 # but impacts the performance for longer length sequences by a slight margin.
 def chunk(X, left, mid, right, strict=False):
+    was_unsqueezed = False
     if left > mid or right > mid:
         return _chunk_extended(X, left, mid, right, strict=strict)
+    # Handling single dimensional inputs which are squeezed.
+    if len(X.shape) == 2:
+        X = X.unsqueeze(2)
+        was_unsqueezed = True
     N, t, d = X.shape
     n_chunks = int(np.ceil(float(t) / mid))
     t_new = n_chunks * mid
@@ -59,11 +64,18 @@ def chunk(X, left, mid, right, strict=False):
     X = torch.cat([X_left, X, X_right], dim=2)
     if strict:
         X = X[:, 1:-1, :, :].contiguous()
+    if was_unsqueezed:
+        X = X.squeeze(3)
     return X, t
 
 # Extended support for chunking when left or right > mid.
 # Not using this by default because using lists worsen the performance by a small amount.
 def _chunk_extended(X, left, mid, right, strict=False):
+    # Handling single dimensional inputs which are squeezed.
+    was_unsqueezed = False
+    if len(X.shape) == 2:
+        X = X.unsqueeze(2)
+        was_unsqueezed = True
     N, t, d = X.shape
     n_chunks = int(np.ceil(float(t) / mid))
     t_new = n_chunks * mid
@@ -73,7 +85,7 @@ def _chunk_extended(X, left, mid, right, strict=False):
     left_reps = int(np.ceil(float(left) / mid))
     right_reps = int(np.ceil(float(right) / mid))
     if strict:
-    	X = torch.nn.functional.pad(X, pad=(0, 0, 0, 0, left_reps, right_reps), mode='constant', value=0)
+        X = torch.nn.functional.pad(X, pad=(0, 0, 0, 0, left_reps, right_reps), mode='constant', value=0)
     X_list = []
     for roll in range(left_reps, -(right_reps+1), -1):
         X_list.append(torch.roll(X, roll, 1))
@@ -81,14 +93,23 @@ def _chunk_extended(X, left, mid, right, strict=False):
     zero_index = mid * left_reps
     X = X[:, :, zero_index - left: zero_index + mid + right, :]
     if strict:
-    	X = X[:, left_reps:n_chunks+left_reps, :, :]
+        X = X[:, left_reps:n_chunks+left_reps, :, :]
+    if was_unsqueezed:
+        X = X.squeeze(3)
     return X, t
 
 # Inverse of the chunking operation.
 # Inputs : chunked tensor from chunk() function, left_context, mid_context, right_context, original_time
 # Output : Tensor of shape same as input to the chunk() function
 def unchunk(X, left, mid, right, t_original):
+    # Handling single dimensional inputs which are squeezed.
+    was_unsqueezed = True
+    if len(X.shape) == 3:
+        X = X.unsqueeze(3)
+        was_unsqueezed = True
     N, n_chunks, size_chunk, d = X.shape
     X = X[:, :, left:left+mid, :].contiguous()
     X = X.view((N, n_chunks * mid, d))[:, :t_original, :]
+    if was_unsqueezed:
+        X = X.squeeze(2)
     return X
